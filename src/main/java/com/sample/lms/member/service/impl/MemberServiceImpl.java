079e5a14112d68d8b9b916d85b4a1122e899114e
SeongHo <tjdgh148@naver.com>
2022-03-22 15:59:03 +0900

    commit

--- src/main/java/com/sample/lms/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/sample/lms/member/service/impl/MemberServiceImpl.java
@@ -1,13 +1,16 @@
-package com.sample.lms.member.service;
+package com.sample.lms.member.service.impl;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.sample.lms.components.MailComponents;
 import com.sample.lms.member.entity.Member;
 import com.sample.lms.member.model.MemberInput;
 import com.sample.lms.member.repository.MemberRepository;
+import com.sample.lms.member.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 public class MemberServiceImpl implements MemberService {
 
 	private final MemberRepository memberRepository;
+	private final MailComponents mailComponents;
 	
 	@Override
 	public boolean register(MemberInput parameter) {
@@ -25,16 +29,44 @@ public class MemberServiceImpl implements MemberService {
 			return false;
 		}
 		
-		Member member = new Member();
-		member.setUserId(parameter.getUserID());
-		member.setUserName(parameter.getUserName());
-		member.setPhone(parameter.getPhone());
-		member.setPassword(parameter.getPassword());
-		member.setRegDt(LocalDateTime.now());
+		String uuid = UUID.randomUUID().toString();
+		
+		Member member = Member.builder()
+				.userId(parameter.getUserID())
+				.userName(parameter.getUserName())
+				.phone(parameter.getPhone())
+				.password(parameter.getPassword())
+				.regDt(LocalDateTime.now())
+				.emailAuthYn(false)
+				.emailAuthKey(uuid)
+				.build();
 		
 		memberRepository.save(member);
 		
-		return false;
+		String email = parameter.getUserID();
+		String subject = "LMS 사이트 가입을 축하드립니다.";
+		String text = "<p>LMS 사이트 가입을 축하드립니다.</p><p>아래 링크를 클릭하셔서 가입을 완료하세요.</p>"
+				+ "<div><a target='_blank' href='http://localhost:8080/member/email-auth?id=" + uuid + "'>가입 완료</a></div>";
+		
+		mailComponents.sendMail(email, subject, text);
+		
+		return true;
+	}
+
+	@Override
+	public boolean emailAuth(String uuid) {
+		
+		Optional<Member> optionalMember = memberRepository.findByEmailAuthKey(uuid);
+		if (!optionalMember.isPresent()) {
+			return false;
+		}
+		
+		Member member = optionalMember.get();
+		member.setEmailAuthYn(true);
+		member.setEmailAuthDt(LocalDateTime.now());
+		memberRepository.save(member);
+		
+		return true;
 	}
 
 }
