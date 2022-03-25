9a5da37636fa773a6901cc2fe725a7c95fe88212
SeongHo <tjdgh148@naver.com>
2022-03-25 22:20:44 +0900

    commit

---
+++ src/main/java/com/sample/lms/member/exception/MemberNotEmailAuthException.java
@@ -0,0 +1,8 @@
+package com.sample.lms.member.exception;
+
+public class MemberNotEmailAuthException extends RuntimeException {
+
+	public MemberNotEmailAuthException(String error) {
+		super(error);
+	}
+}
