951f3c4dd6051a5637e4530b5d762c4bff01d041
SeongHo <tjdgh148@naver.com>
2022-03-29 19:47:28 +0900

    commit

--- src/main/java/com/sample/lms/member/service/MemberService.java
+++ src/main/java/com/sample/lms/member/service/MemberService.java
@@ -9,8 +9,24 @@ public interface MemberService extends UserDetailsService {
 
 	boolean register(MemberInput parameter);
 	
+	/*
+	 * uuid에 해당하는 계정을 활성화
+	 */
 	boolean emailAuth(String uuid);
 
+	/*
+	 * 입력한 이메일로 비밀번호 재설정 정보 전송
+	 */
 	boolean sendResetPassword(ResetPasswordInput parameter);
+
+	/*
+	 * 입력받은 uuid에 대해서 password로 재설정
+	 */
+	boolean resetPassword(String uuid, String password);
+
+	/*
+	 * 입력받은 uuid값이 유효한지 확인
+	 */
+	boolean checkResetPassword(String uuid);
 	
 }
